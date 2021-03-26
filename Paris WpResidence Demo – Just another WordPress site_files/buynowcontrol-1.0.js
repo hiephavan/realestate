4eb04404d64bc51751b67c1f309f0942544bd95e
Macbook <macbook@Macbooks-MacBook-Pro-2.local>
2021-03-26 09:40:51 +0700

    Second1  Commit

--- Paris WpResidence Demo – Just another WordPress site_files/buynowcontrol-1.0.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function ($) {
-    "use strict";
-
-    jQuery('#wpestate_buy_now_footerclose').on('click',function(){
-        jQuery('.wpestate_buy_now_footer').hide();
-    });
-    
-    
-});
